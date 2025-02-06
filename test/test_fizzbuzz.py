8bef0409c7354d4cdec9c9c9e2081e00353b44f6
Faycal Boubekri <88287439+ooFaycaloo@users.noreply.github.com>
2025-02-06 16:01:30 +0100

    comit4

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -1,35 +1,29 @@
-
 import unittest
+from io import StringIO
+import sys
 from fizzbuzz import fizzbuzz
 
 
 class TestFizzBuzz(unittest.TestCase):
-
     def test_fizzbuzz(self):
-        test_cases = [
-            (1, "1"),
-            (2, "2"),
-            (3, "Fizz"),
-            (5, "Buzz"),
-            (6, "Fizz"),
-            (10, "Buzz"),
-            (15, "FizzBuzz"),
-            (30, "FizzBuzz"),
-            (45, "FizzBuzz"),
-            (7, "7"),
-            (8, "8"),
-            (78, "Fizz"),
-            (12, "Fizz"),
-            (20, "Buzz"),
-            (25, "Buzz"),
-            (50, "Buzz"),
-            (90, "FizzBuzz"),
+        # Capture la sortie standard
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        # Exécute la fonction
+        fizzbuzz(15)
+
+        # Récupère la sortie
+        sys.stdout = sys.__stdout__
+        output = captured_output.getvalue().splitlines()
+
+        # Vérifie les résultats attendus
+        expected_output = [
+            "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
+            "11", "Fizz", "13", "14", "FizzBuzz"
         ]
-
-        for n, expected in test_cases:
-            with self.subTest(n=n):
-                self.assertEqual(fizzbuzz(n), expected)
+        self.assertEqual(output, expected_output)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
